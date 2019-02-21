153d278c935f86ef38a62fdf34d1487e6fa8db47
Matteo Scandolo <teo@opennetworking.org>
2019-02-21 15:13:35 -0800

    [SEBA-494] Setting required core version
    Change-Id: Ifae6ed2e722fb874bd57e02c76e3821d30da879e

--- xos/synchronizer/simpleexampleservice-synchronizer.py
+++ xos/synchronizer/simpleexampleservice-synchronizer.py
@@ -20,14 +20,11 @@ import os
 from xossynchronizer import Synchronizer
 from xosconfig import Config
 
-base_config_file = os.path.abspath(os.path.dirname(
-    os.path.realpath(__file__)) + '/config.yaml')
-mounted_config_file = os.path.abspath(os.path.dirname(
-    os.path.realpath(__file__)) + '/mounted_config.yaml')
+base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
 
 if os.path.isfile(mounted_config_file):
-    Config.init(base_config_file, 'synchronizer-config-schema.yaml',
-                mounted_config_file)
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
 else:
     Config.init(base_config_file, 'synchronizer-config-schema.yaml')
 
